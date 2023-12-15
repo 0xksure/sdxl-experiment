2f60ea38388f2ce22e73e7441300847dcc771d33
0xksure <kristoffer.hovland.berg@gmail.com>
2023-12-15 10:55:41 +0100

    update prompt

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import torch
 pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
 pipe.to("cuda")
 
-prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
+prompt = "Cinematic shot of paw patrollers on their way to save chicaletta once more"
 
 image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
 image.save("test.png")
