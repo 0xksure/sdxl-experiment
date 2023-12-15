c81b381362d3f64c811bc6b34e6b2e5f081fe87b
Kristoffer <kristoffer.hovland.berg@gmail.com>
2023-12-15 15:02:59 +0100

    add img2img

--- src/img2img.py
+++ src/img2img.py
@@ -11,5 +11,5 @@ url = "./dfod.jpg"
 init_image = load_image(url).convert("RGB")
 prompt = "Dog in a trippy void, same drawing style, high quality, 8k"
 negative_prompt = "Low quality, blurry image."
-image = pipe(prompt, negative_prompt, image=init_image).images[0]
+image = pipe(prompt, negative_prompt, strength=0.8, image=init_image).images[0]
 image.save("dfod_1.png")
