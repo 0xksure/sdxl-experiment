50341910b1daca3e1d8365b00702ca4c9130db8c
Kristoffer <kristoffer.hovland.berg@gmail.com>
2023-12-15 11:41:14 +0100

    init image:

--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ import torch
 pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
 pipe.to("cuda")
 
+init_image = load_image("./pp.jpeg").resize((512, 512))
+
 prompt = "Cinematic shot of paw patrollers on their way to save chicaletta once more"
 
-image = pipe(prompt=prompt, num_inference_steps=100, guidance_scale=0.0).images[0]
+image = pipe(prompt=prompt, image=init_image,num_inference_steps=100, guidance_scale=0.0).images[0]
 image.save("test.png")
