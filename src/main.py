c004047ee518a853671d0483261de1efc7beea2c
0xksure <kristoffer.hovland.berg@gmail.com>
2023-12-15 11:48:02 +0100

    ping pong

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ pipe.to("cuda")
 
 init_image = load_image("./pp.jpeg").resize((512, 512))
 
-prompt = "Cinematic shot of paw patrollers on their way to save chicaletta once more"
+prompt = "Cinematic shot of paw patrollers on their way to save chicaletta once more. Make sure that the eyes are photo realistic."
 
-image = pipe(prompt=prompt, image=init_image,num_inference_steps=2, guidance_scale=0.0).images[0]
+image = pipe(prompt=prompt, image=init_image,num_inference_steps=25, guidance_scale=0.0).images[0]
 image.save("test.png")
