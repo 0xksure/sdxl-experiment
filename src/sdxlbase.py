986e334bcaefebf9c7efd8752f4db207961346fb
0xksure <kristoffer.hovland.berg@gmail.com>
2023-12-15 13:10:37 +0100

    update

--- src/sdxlbase.py
+++ src/sdxlbase.py
@@ -1,5 +1,5 @@
 import torch
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler,StableDiffusionUpscalePipeline
 from diffusers.utils import load_image
 model_id = "stabilityai/stable-diffusion-2-1"
 
@@ -7,22 +7,30 @@ model_id = "stabilityai/stable-diffusion-2-1"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
+pipe.enable_attention_slicing()
 
-
-n_steps = 40
+n_steps = 50 
 high_noise_frac = 0.8
 
 init_image = load_image("./pp.jpeg").resize((512, 512))
 
 
-prompt = "Cinematic shot of paw patrollers on their way to save chicaletta once more. Make sure that the eyes are photo realistic."
+prompt = "Paw patrol on a mission to rescue chicaletta"
+negative_prompt = "Low quality, blurry image."
 image = pipe(
-    prompt=prompt, 
+    prompt=prompt,
+    negative_prompt=negative_prompt, 
     image=init_image,
     num_inference_steps=n_steps,
     denoising_end=high_noise_frac,
 ).images[0]
 
+model_id = "stabilityai/stable-diffusion-x4-upscaler"
+pipeline = StableDiffusionUpscalePipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipeline = pipeline.to("cuda")
 
+prompt = "Paw Patrol on a mission "
+negative_prompt="bad, deformed, ugly, bad anotomy"
+image = pipe(prompt=prompt, image=image, negative_prompt=negative_prompt, strength=0.7).images[0]
     
 image.save("astronaut_rides_horse.png")
