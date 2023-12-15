55cb99d89e5f2f305e5b0149906d8fc7e8a78251
Kristoffer <kristoffer.hovland.berg@gmail.com>
2023-12-15 12:55:25 +0100

    update prediction

--- src/sdxlbase.py
+++ src/sdxlbase.py
@@ -8,15 +8,6 @@ pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to("cuda")
 
-refiner = StableDiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-refiner-1.0",
-    text_encoder=pipe.text_encoder_2,
-    vae=pipe.vae,
-    torch_dtype=torch.float16,
-    use_safetensors=True,
-    variant="fp16",
-)
-refiner.to("cuda")
 
 n_steps = 40
 high_noise_frac = 0.8
@@ -33,11 +24,6 @@ image = pipe(
     output_type="latent",
 ).images[0]
 
-image = refiner(
-    prompt=prompt,
-    num_inference_steps=n_steps,
-    denoising_start=high_noise_frac,
-    image=image,
-).images[0]
+
     
 image.save("astronaut_rides_horse.png")
